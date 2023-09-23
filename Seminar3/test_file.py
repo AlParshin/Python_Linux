c475b90c65dd3f6c97e33974e41225d917cf256f
Alexey Parshin <jax77@mail.ru>
2023-09-23 17:21:35 +0300

    Выполнил домашнее задание по семинару 3 (linux).

--- Seminar3/test_file.py
+++ Seminar3/test_file.py
@@ -13,32 +13,23 @@ def test_step1():
     conftest.logger('test_step1')
 
 
-if __name__ == '__main__':
-    pytest.main(['-vv'])
-
-
-def test_step1():
-    assert checkout(f'cd {folderin}; 7z a {folderout}/arh1',
-                    'Everything is OK'), 'test_step1 FAIL'
-
-
 def test_step2():
-    assert checkout(f'cd {folderout}; 7z d arh1.7z',
+    assert checkout(f"cd {data['folderout']}; 7z d arh1.7z",
                     'Everything is OK'), 'test_step2 FAIL'
 
 
 def test_step3():
-    assert checkout(f'cd {folderext}; 7z u {folderout}/arh1',
+    assert checkout(f"cd {data['folderext']}; 7z u {data['folderout']}/arh1",
                     'Everything is OK'), 'test_step3 FAIL'
 
 
 def test_step4():
     assert checkout_list_of_files(
-        f'cd {folderin}; 7z l {folderout}/arh1', 'Everything is OK'), 'test_step4 FAIL'
+        f"cd {data['folderin']}; 7z l {data['folderout']}/arh1", 'Everything is OK'), 'test_step4 FAIL'
 
 
 def test_step5():
-    assert checkout(f'cd {folderin}; 7z x {folderout}/arh1',
+    assert checkout(f"cd {data['folderin']}; 7z x {data['folderout']}/arh1",
                     'Everything is OK'), 'test_step5 FAIL'
 
 
